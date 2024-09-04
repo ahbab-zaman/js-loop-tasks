7238d2d9bbb37dc9a26eceb249da8652942b4fa1
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-09-05 02:45:45 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,41 @@
+// Task 1
+
+// let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const reverse = [];
+// for(const items of colors){
+//   reverse.unshift(items);
+//   console.log(items);
+// }
+// console.log(reverse);
+
+// Task 2
+
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// let even = [];
+// for(i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 2 === 0) {
+//        even.unshift(numbers[i]);
+//     }
+// }
+// console.log(even);
+
+// Task 3
+
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// // let add = numbers.split('').join('');
+// let add = (numbers.join(''));
+// console.log(add);
+
+// Task 4
+
+// const statement = 'I am a hard working person';
+// let str = statement.split(" ").reverse().join(" ");
+
+
+//*** If only use double quotes without space than the whole sentence will reversed as well as the words.
+//*** If use use double quotes with white space then the only the sentence will reverse not the words.
+
+
+// console.log(str);
+
+
---
+++ more-practice.js
@@ -0,0 +1,98 @@
+//*************************//
+
+// Task 1
+
+// const numbers = [3, 45, 12, 67, 12];
+// let sum = 0;
+// for(num of numbers){
+//     sum += num;
+// }
+// console.log(`Total sum of the numbers ${sum}`);
+
+// Task 2
+
+// maximum number
+// const numbers = [3, 45, 12, 67, 12];
+// let maxNum = numbers[0];
+// for(i = 0; i <= numbers.length; i++){
+//     if (maxNum < numbers[i]) {
+//         maxNum = numbers[i];
+//     }
+// }
+// console.log(maxNum);
+
+// minimum number
+
+// const numbers = [122, 123, 41, 14, 102, 24, 424, 421];
+// let minNum = numbers[0];
+// for(i = 0; i < numbers.length; i++){
+//     if (numbers[i] < minNum) {
+//         minNum = numbers[i];
+//     }
+// }
+// console.log(minNum);
+
+
+// Task 3
+
+// const numbers = [23, 43, 32, 324, 10, 89];
+// let odd = [];
+// for(num of numbers){
+//     if (num % 2 === 1) {
+//        odd.push(num);
+//     }
+// console.log(odd);
+// }
+
+
+// Task 4
+
+// const numbers = [3, 5,4,14,6,10];
+// let reversed = numbers.reverse();
+// console.log(reversed);
+
+
+// Task 5
+
+// const numbers = [12, 24, 24, 15, 455, 45, 1, 5, 19];
+// const newNum = [];
+// for(num of numbers){
+//     if(num > 15){
+//         newNum.push(num);
+//     }
+// }
+// console.log(newNum);
+
+
+// Task 6
+
+// const numbers = [12, 24, 24, 15, 455, 45, 1, 5, 19];
+// let sum = 0;
+// for(num of numbers){
+//     sum += num;
+// }
+// let average = sum / numbers.length;
+// console.log(average);
+
+
+// Task 7
+
+// const numbers1 = [1, 2, 3];
+// const numbers2 = [3, 4, 5];
+// const merge = numbers1.concat(numbers2);
+// console.log(merge);
+
+
+// Task 8
+
+//*** Find the prime number
+
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+let primeNum = [];
+for(num of numbers){
+    if (num % num === 0 && num ) {
+        
+    }
+}
+
+//*************************//
